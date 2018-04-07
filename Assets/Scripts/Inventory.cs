815efd4f6cfa7aa8bf7e3a3465358432effc2341
Scott Porter <scott.porter.ii@gmail.com>
2018-04-07 18:20:04 -0500

    Feature/11 - Inventory System (#54)
    * Added Canvas UI and Inventory Bar with one element
    
    * Auto numbering of inventory slot logic
    
    * Item Interface and example item created
    
    * Can add items to inventory panel
    
    * Can set currently selected item for use
    
    * Can select inventory with numbers

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    public GameObject SlotPrefab;
+    public GameObject ExamplePrefab;
+    public IItem SelectedIventoryItem;  // Set by slots onClick
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            var go = Instantiate(ExamplePrefab);
+            AddInventory(go.GetComponent<IItem>());
+        }
+    }
+
+    void AddInventory(IItem item)
+    {
+        var slot = Instantiate(SlotPrefab, transform).GetComponent<InventorySlot>();
+        slot.Item = item;
+    }
+}
---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour {
+    public Text NumberTextElement;
+    public Image ImageIconElement;
+
+    private IItem item;
+    public IItem Item { get { return item; } set { SetItem(value); } }
+
+    int inventoryNumber = -1;
+    Inventory inventory;
+    Button button;
+
+	// Use this for initialization
+	void Awake () {
+        InitializeSlot();
+        inventory = transform.parent.GetComponent<Inventory>();
+        button = GetComponentInChildren<Button>();
+
+        if(inventoryNumber > 9)
+        {
+            Debug.LogError("Can not have more than 9 items in inventory.  Should this ever happen in our game?  Destroying slot and item");
+            Destroy(gameObject);
+        }
+	}
+
+    void OnValidate()
+    {
+        InitializeSlot();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(inventoryNumber.ToString()))
+        {
+            button.Select();
+            SlotSelected();
+        }
+    }
+
+    void InitializeSlot()
+    {
+        if (transform == null || transform.parent == null)
+            return;
+        
+        var slots = transform.parent.GetComponentsInChildren<InventorySlot>();
+
+        for (int i = 0; i < slots.Length; ++i)
+        {
+            if (slots[i] == this)
+            {
+                SetInventoryNumber(i + 1);
+            }
+        }
+    }
+	
+	void SetInventoryNumber(int i)
+    {
+        inventoryNumber = i;
+        NumberTextElement.text = i.ToString();
+    }
+
+    void SetItem(IItem newItem)
+    {
+        ImageIconElement.sprite = newItem.InventoryIcon;
+        item = newItem;
+        item.gameObject.transform.SetParent(transform, true);
+        item.gameObject.transform.position = new Vector2(5000 + inventoryNumber * 100, 0);  // Just move it far out of the way
+
+    }
+
+    public void SlotSelected()
+    {
+        inventory.SelectedIventoryItem = item;
+        Debug.Log(item.gameObject + " selected for use");
+    }
+
+}
