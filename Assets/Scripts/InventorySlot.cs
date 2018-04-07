0185023225fca966b586d25ecee219e9514363f1
saporter_mac <scott.porter.ii@gmail.com>
2018-04-07 15:48:21 -0500

    Auto numbering of inventory slot logic

---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour {
+    public Text NumberTextElement;
+
+    //int inventoryNumber = -1;
+
+	// Use this for initialization
+	void Awake () {
+        InitializeSlot();
+	}
+
+    void OnValidate()
+    {
+        InitializeSlot();
+    }
+
+    void InitializeSlot()
+    {
+        if (transform == null || transform.parent == null)
+            return;
+        
+        var slots = transform.parent.GetComponentsInChildren<InventorySlot>();
+
+        for (int i = 0; i < slots.Length; ++i)
+        {
+            if (slots[i] == this)
+            {
+                SetInventoryNumber(i + 1);
+            }
+        }
+    }
+	
+	void SetInventoryNumber(int i)
+    {
+        //inventoryNumber = i;
+        NumberTextElement.text = i.ToString();
+    }
+}
