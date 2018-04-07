3fdf010910f4903106b9ed6bd5e5c1829b3c6200
saporter_mac <scott.porter.ii@gmail.com>
2018-04-07 17:13:56 -0500

    Item Interface and example item created

---
+++ Assets/Scripts/IItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IItem {
+    GameObject gameObject { get; }  // This is defined by MonoBehaviour class, so you shouldn't have to explicitly implement in your item class
+    Sprite InventoryIcon { get; }   // The sprite that appears in the inventory panel when item is picked up
+    void UseAt(GameObject location);// passing Gameobject instead of Transform to give more access if needed.  This is the in game location an item should be used
+}
