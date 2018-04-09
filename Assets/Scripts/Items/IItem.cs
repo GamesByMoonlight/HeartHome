2082377b65367dfed520126b466eb0e0ae8788b2
saporter_mac <scott.porter.ii@gmail.com>
2018-04-08 20:42:28 -0500

    Mouse to use items added.  Paintbrush as quick example started.

--- Assets/Scripts/Items/IItem.cs
+++ Assets/Scripts/Items/IItem.cs
@@ -6,4 +6,5 @@ public interface IItem {
     GameObject gameObject { get; }  // This is defined by MonoBehaviour class, so you shouldn't have to explicitly implement in your item class
     Sprite InventoryIcon { get; }   // The sprite that appears in the inventory panel when item is picked up
     void UseAt(GameObject location);// passing Gameobject instead of Transform to give more access if needed.  This is the in game location an item should be used
+    void UseAt(Vector2 location);
 }
