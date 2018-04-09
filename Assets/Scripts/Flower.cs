ef3d3c5f19ff1fd85a3aebbb507bfd357abaaa8d
saporter_mac <scott.porter.ii@gmail.com>
2018-04-08 20:03:39 -0500

    Event system added and event raised when time to make a Ghost

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -5,7 +5,28 @@ using UnityEngine;
 // Script to identify flower objects, if needed, and to add behaviors later
 
 public class Flower : MonoBehaviour {
+    public static Flower OldestFlower;
+
+    private static Flower YoungestFlower;
     private static int FlowersCreated = 0;
+    private static bool GhostMade = false;  // Some extra reduntancy 
+
+    Flower Next;
+
+    private void Awake()
+    {
+        // Create a linked list
+        if (OldestFlower == null)
+        {
+            OldestFlower = this;
+        }
+
+        if (YoungestFlower != null)
+        {
+            YoungestFlower.Next = this;
+        }
+        YoungestFlower = this;
+    }
 
     private void Start()
     {
@@ -14,5 +35,11 @@ public class Flower : MonoBehaviour {
             GetComponent<FlowerGrowth>().PlayAudioClip();
         }
         FlowersCreated++;
+
+        if(FlowersCreated >= 40 && !GhostMade)
+        {
+            GhostMade = true;
+            GameEventSystem.Instance.MakeGhost.Invoke();
+        }
     }
 }
