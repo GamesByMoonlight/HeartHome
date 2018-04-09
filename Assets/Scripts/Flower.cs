38bf4ed3094c3ffdae255e2435cd3219cd618971
Scott Porter <scott.porter.ii@gmail.com>
2018-04-08 20:05:15 -0500

    Feature/58 - Flowers finished (#62)
    * Fanfare plays on first flower only
    
    * Event system added and event raised when time to make a Ghost

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -5,5 +5,41 @@ using UnityEngine;
 // Script to identify flower objects, if needed, and to add behaviors later
 
 public class Flower : MonoBehaviour {
+    public static Flower OldestFlower;
 
+    private static Flower YoungestFlower;
+    private static int FlowersCreated = 0;
+    private static bool GhostMade = false;  // Some extra reduntancy 
+
+    Flower Next;
+
+    private void Awake()
+    {
+        // Create a linked list
+        if (OldestFlower == null)
+        {
+            OldestFlower = this;
+        }
+
+        if (YoungestFlower != null)
+        {
+            YoungestFlower.Next = this;
+        }
+        YoungestFlower = this;
+    }
+
+    private void Start()
+    {
+        if(FlowersCreated == 0)
+        {
+            GetComponent<FlowerGrowth>().PlayAudioClip();
+        }
+        FlowersCreated++;
+
+        if(FlowersCreated >= 40 && !GhostMade)
+        {
+            GhostMade = true;
+            GameEventSystem.Instance.MakeGhost.Invoke();
+        }
+    }
 }
---
+++ Assets/Scripts/TestMakeGhostScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMakeGhostScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if(GameEventSystem.Instance != null)
+            GameEventSystem.Instance.MakeGhost.AddListener(MakeGhost);
+	}
+
+    void MakeGhost()
+    {
+        Debug.Log("Make Ghost!");
+    }
+
+    private void OnDestroy()
+    {
+        if(GameEventSystem.Instance != null)
+        {
+            GameEventSystem.Instance.MakeGhost.RemoveListener(MakeGhost);
+        }
+    }
+
+}
