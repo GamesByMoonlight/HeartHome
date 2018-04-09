8ff99ba82f6da7e21db41662362175c0a7c0847b
Scott Porter <scott.porter.ii@gmail.com>
2018-04-08 20:46:50 -0500

    Feature/5 - mouse (#63)
    * Mouse to use items added.  Paintbrush as quick example started.
    
    * Changed Flower to trigger.  Moved player to sort order layer 1 (drawn on top of other sprites)

---
+++ Assets/Scripts/Items/Paintbrush.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paintbrush : Item {
+    public GameObject FlowerPrefab;
+
+    public override void UseAt(GameObject location)
+    {
+        UseAt(location.transform.position);
+    }
+
+    public override void UseAt(Vector2 location)
+    {
+        var Flower = Instantiate(FlowerPrefab);
+        Flower.transform.position = location;
+
+        Debug.Log("Make Flower at " + location);
+    }
+}
---
+++ Assets/Scripts/PlayerControls/MouseAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAction : MonoBehaviour {
+    public float MouseDownLag = .2f;    // How many seconds we should wait before registering another mouse click
+
+    float lastMouseDown = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForAction();
+    }
+
+    void CheckForAction()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if(Time.time - lastMouseDown > MouseDownLag)
+            {
+                lastMouseDown = Time.time;
+                if(Inventory.Current.SelectedIventoryItem != null)
+                    Inventory.Current.SelectedIventoryItem.UseAt(Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y)));
+            }
+        }
+    }
+}
