20fc3f7eb2a9e2dce9c34b530242620531d9811f
Scott Porter <scott.porter.ii@gmail.com>
2018-04-10 17:08:00 -0500

    Added DirectionAperture property to PlayerAction.cs for use by items (#65)

--- Assets/Scripts/PlayerControls/PlayerAction.cs
+++ Assets/Scripts/PlayerControls/PlayerAction.cs
@@ -2,65 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAction : MonoBehaviour {
+public class PlayerAction : MonoBehaviour
+{
     public GameObject DetectedItem;
     [SerializeField] GameObject DownAperature;  // The "front" of the player.  For use in using items at correct location
     [SerializeField] GameObject RightAperature;  // The "front" of the player.  For use in using items at correct location
     [SerializeField] GameObject LeftAperature;  // The "front" of the player.  For use in using items at correct location
     [SerializeField] GameObject UpAperature;  // The "front" of the player.  For use in using items at correct location
 
+    public GameObject DirectionAperature { get { return GetDirection(); } }
+
     CharacterMovement Movement;
 
-	// Use this for initialization
-	void Awake () {
+    // Use this for initialization
+    void Awake()
+    {
         Movement = GetComponent<CharacterMovement>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		CheckForAction();
-	}
+    }
 
-	void CheckForAction() {
-        if(Input.GetButtonDown("Action"))
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForAction();
+    }
+
+    void CheckForAction()
+    {
+        if (Input.GetButtonDown("Action"))
         {
-			if (DetectedItem == null) {
-                if(Inventory.Current.SelectedIventoryItem != null)
-                {
-                    switch(Movement.Direction)
-                    {
-                        case FacingDirection.Up:
-                            Inventory.Current.SelectedIventoryItem.UseAt(UpAperature);
-                            break;
-                        case FacingDirection.Down:
-                            Inventory.Current.SelectedIventoryItem.UseAt(DownAperature);
-                            break;
-                        case FacingDirection.Left:
-                            Inventory.Current.SelectedIventoryItem.UseAt(LeftAperature);
-                            break;
-                        case FacingDirection.Right:
-                            Inventory.Current.SelectedIventoryItem.UseAt(RightAperature);
-                            break;
-                    }
-                    //Inventory.Current.SelectedIventoryItem.UseAt(Aperature);
-                }
+            if (DetectedItem == null)
+            {
+                UseItem(DirectionAperature);
                 return;
-			}
+            }
 
-			var item = DetectedItem.GetComponent<IItem>();
-			if (item == null) {
-				return;
-			}
+            var item = DetectedItem.GetComponent<IItem>();
+            if (item == null)
+            {
+                UseItem(DetectedItem);
+                return;
+            }
 
             Inventory.Current.AddInventory(DetectedItem.GetComponent<IItem>());
-        }		
-	}
+        }
+    }
 
-	void OnTriggerEnter2D(Collider2D collider) {
-		this.DetectedItem = collider.gameObject;	
-	}
+    void UseItem(GameObject location)
+    {
+        if (Inventory.Current.SelectedIventoryItem != null)
+        {
+            Inventory.Current.SelectedIventoryItem.UseAt(location);
+        }
+    }
 
-	void OnTriggerExit2D(Collider2D collider) {
-		this.DetectedItem = null;	
-	}	
+    GameObject GetDirection()
+    {
+        GameObject Aperature = null;
+        switch (Movement.Direction)
+        {
+            case FacingDirection.Up:
+                Aperature = UpAperature;
+                break;
+            case FacingDirection.Down:
+                Aperature = DownAperature;
+                break;
+            case FacingDirection.Left:
+                Aperature = LeftAperature;
+                break;
+            case FacingDirection.Right:
+                Aperature = RightAperature;
+                break;
+            default:
+                Aperature = DownAperature;
+                break;
+        }
+
+        return Aperature;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        this.DetectedItem = collider.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        this.DetectedItem = null;
+    }
 }
