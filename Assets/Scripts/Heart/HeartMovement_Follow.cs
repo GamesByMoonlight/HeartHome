55d1a7c1bbbfdb63cda64d00fd113c10581ed8e1
ZaneEverett <36215239+Zaneeverett@users.noreply.github.com>
2018-04-09 21:47:20 -0500

    Basic heart follow mechanic.

---
+++ Assets/Scripts/Heart/HeartMovement_Follow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartMovement_Follow : MonoBehaviour {
+
+    public float DistanceFollowed = 0.0f;
+    public float MoveSpeed = 0.0f;
+    public Transform followTarget;
+    public float dist = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+
+        dist = Vector3.Distance(followTarget.position, transform.position);
+
+        if (dist > DistanceFollowed)
+        {
+            float step = MoveSpeed * Time.deltaTime;
+            //transform.position = Vector3.Lerp(followTarget.position, transform.position, step);
+            transform.position = Vector3.MoveTowards(transform.position, followTarget.position, step);
+        }
+
+    }
+}
