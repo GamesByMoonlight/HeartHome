bc71bd6289330ee229f7d65344ca6b018750e33b
Luis Davila <ldavila07@icloud.com>
2018-04-09 22:10:55 -0500

    windows build config complete

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -10,15 +10,5 @@ echo "Attempting to build ${project} for Windows"
   -executeMethod "$(pwd)/Build/windows/${project}.exe"  \
   -quit
 
-  #echo "Attempting to build $project for Windows"
-#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-#  -batchmode \
-#  -nographics \
-#  -silent-crashes \
-#  -logFile \
-#  -projectPath $(pwd)/ \
-#  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-#  -quit
-
 echo 'Logs from build'
 cat $(pwd)/unity.log
