5f3bc5943f181f4623d0db166b9f5d368f434dfc
LAPTOP-NHNQ0FS3\cisco <ciscom1971@yahoo.com>
2018-04-10 21:33:07 -0500

    added fertile soil  and seeds prefab

---
+++ Assets/Scripts/FertileSoil.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FertileSoil : MonoBehaviour {
+
+    public GameObject ThrownSeedsPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlantSeeds()
+    {
+        var seeds = Instantiate(ThrownSeedsPrefab, transform.position, Quaternion.Euler(0f, 0f, 0f), transform);
+        //seeds.transform.position = transform.position;
+
+
+
+    }
+}
