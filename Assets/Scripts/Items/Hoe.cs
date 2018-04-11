fe456bc4bd7d322a43020c321d81ff4c2fc0dcf6
Luis Davila <ldavila07@icloud.com>
2018-04-10 20:03:49 -0500

    Created Hoe Game Object (#67)

---
+++ Assets/Scripts/Items/Hoe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoe : Item {
+
+	private PlayerAction playerAction;
+	public GameObject FertileSoilPrefab;
+
+	void Awake() {
+		playerAction = GameObject.Find ("Player").GetComponent<PlayerAction>();
+
+		if (playerAction == null) {
+			Debug.LogError("PlayerAction not found in scene. Hoe needs it");
+		}
+	
+
+	}
+
+	public override void UseAt (GameObject location)
+	{
+		var soil = Instantiate (FertileSoilPrefab);
+		soil.transform.position = playerAction.DirectionAperature.transform.position;
+	}
+
+	// Use this for initialization
+
+}
