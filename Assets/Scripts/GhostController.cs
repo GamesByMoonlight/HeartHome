8cac6bc5d3b9cb27f01c3c491572d5f9a91a6ade
saporter_mac <scott.porter.ii@gmail.com>
2018-04-12 12:39:54 -0500

    Finished Ghost AnimatorController

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -6,10 +6,12 @@ public class GhostController : MonoBehaviour {
     public float Speed = 5f;
 
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,9 +20,34 @@ public class GhostController : MonoBehaviour {
         var  vertical = Input.GetAxis("Vertical");
         var movement = new Vector2(horizontal, vertical);
 
-        //Debug.Log(Vector2.SignedAngle(Vector2.up, movement));
-
+        Debug.Log(Vector2.SignedAngle(Vector2.up, movement));
+        var angle = Vector2.SignedAngle(Vector2.up, movement);
+        SetAnimator(angle);
 
         rb.velocity = movement * Speed;
 	}
+
+    void SetAnimator(float angle)
+    {
+        if (Mathf.Abs(angle) < 45f)
+        {
+            animator.SetInteger("Vertical", 1);
+            animator.SetInteger("Horizontal", 0);
+        }
+        else if (Mathf.Abs(angle) > 135f)
+        {
+            animator.SetInteger("Vertical", -1);
+            animator.SetInteger("Horizontal", 0);
+        }
+        else if (angle >= 45f && angle <= 135f)
+        {
+            animator.SetInteger("Vertical", 0);
+            animator.SetInteger("Horizontal", -1);
+        }
+        else if (angle <= -45f && angle >= -135f)
+        {
+            animator.SetInteger("Vertical", 0);
+            animator.SetInteger("Horizontal", 1);
+        }
+    }
 }
