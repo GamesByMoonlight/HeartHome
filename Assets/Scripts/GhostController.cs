d7c8f2713820aeb4fd149f9fb063b437995c8b80
saporter_mac <scott.porter.ii@gmail.com>
2018-04-11 22:58:47 -0500

    Test Ghost Controller added

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour {
+    public float Speed = 5f;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        var horizontal = Input.GetAxis("Horizontal");
+        var  vertical = Input.GetAxis("Vertical");
+        var movement = new Vector2(horizontal, vertical);
+
+        //Debug.Log(Vector2.SignedAngle(Vector2.up, movement));
+
+
+        rb.velocity = movement * Speed;
+	}
+}
