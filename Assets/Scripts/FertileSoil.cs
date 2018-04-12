d33eb8e41e92446bc46e3e857605e07393a6bd18
saporter_mac <scott.porter.ii@gmail.com>
2018-04-11 21:32:07 -0500

    Fertile Soil complete.  Produces Flower once watered WateringCan sprite added with functionality

--- Assets/Scripts/FertileSoil.cs
+++ Assets/Scripts/FertileSoil.cs
@@ -1,24 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FertileSoil : MonoBehaviour {
 
     public GameObject ThrownSeedsPrefab;
+    public GameObject FlowerPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    bool seedsPlanted = false;
+    GameObject seeds;
 
     public void PlantSeeds()
     {
-        var seeds = Instantiate(ThrownSeedsPrefab, transform.position, Quaternion.Euler(0f, 0f, 0f), transform);
+        seedsPlanted = true;
+        seeds = Instantiate(ThrownSeedsPrefab, transform.position, Quaternion.Euler(0f, 0f, 0f), transform);
 
+    }
+
+    internal void WaterSoil()
+    {
+        if (!seedsPlanted)
+            return;
+        
+        var flower = Instantiate(FlowerPrefab, transform.position, Quaternion.Euler(0f, 0f, 0f), transform);
+        Destroy(seeds);
     }
 }
---
+++ Assets/Scripts/Items/WateringCan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WateringCan : Item {
+    private PlayerAction playerAction;
+
+    void Awake()
+    {
+        playerAction = GameObject.Find("Player").GetComponent<PlayerAction>();
+
+        if (playerAction == null)
+        {
+            Debug.LogError("PlayerAction not found in scene. Hoe needs it");
+        }
+
+
+    }
+
+    public override void UseAt(GameObject location)
+    {
+        var soil = location.GetComponent<FertileSoil>();
+        if (soil != null)
+        {
+            soil.WaterSoil();
+        }
+    }
+}
