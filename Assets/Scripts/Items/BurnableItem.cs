47a0329f6d105b073acbec9aa27fbe8e4195d8a9
ciscom1971 <38115032+ciscom1971@users.noreply.github.com>
2018-04-12 21:29:17 -0500

    Feature/32 - Crown only (#74)
    * feature/32
    
    * crown prefab added for feature/32
    
    * added BurnableItem feature/32

---
+++ Assets/Scripts/Items/BurnableItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnableItem : Item {
+
+
+
+    public override void UseAt(GameObject location)
+    {
+
+
+        var firePlace = location.GetComponent<FirePlace>();
+        if (firePlace != null)
+        {
+            firePlace.Burn();
+            Inventory.Current.RemoveInventory(this);
+        }
+    }
+
+}
