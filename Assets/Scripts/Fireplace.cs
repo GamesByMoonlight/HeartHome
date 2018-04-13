589f47ff9013ef836176fa2d1deb51c7ee3d263d
fregas <me@craigscode.com>
2018-04-12 21:43:00 -0500

    merged in fireplace stuff

--- Assets/Scripts/Fireplace.cs
+++ Assets/Scripts/Fireplace.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BurnItem : MonoBehaviour {
+public class FirePlace : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +13,11 @@ public class BurnItem : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void Burn()
+    {
+        Debug.Log("Scott help ");
+        throw new System.NotImplementedException();
+
+    }
 }
