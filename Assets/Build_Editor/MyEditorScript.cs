1c1c1de45d4ea8e90047744eeb9a19a38ff3b636
Luis Davila <ldavila07@icloud.com>
2018-04-13 13:47:31 -0500

    added location path

--- Assets/Build_Editor/MyEditorScript.cs
+++ Assets/Build_Editor/MyEditorScript.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEditor;
-class MyEditorScript
+public class MyEditorScript
 {
-	static void PerformBuild ()
+	public static void PerformBuild ()
 	{
 		string[] scenes = { "Assets/_Scenes/Area 1.unity" };
 		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
 		buildPlayerOptions.scenes = scenes;
-		buildPlayerOptions.locationPathName = "winBuild";
+		buildPlayerOptions.locationPathName = "HeartHome.exe";
 		buildPlayerOptions.target = BuildTarget.StandaloneWindows;
 		buildPlayerOptions.options = BuildOptions.None;
 		BuildPipeline.BuildPlayer(buildPlayerOptions);
