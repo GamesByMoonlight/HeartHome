cce3901869dac5a157d088cfbff4bc8f62c7539b
Luis Davila <ldavila07@icloud.com>
2018-04-13 11:07:43 -0500

    removed silent crash script

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,7 +4,6 @@ echo "Attempting to build ${project} for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd)/ \
   -executeMethod "$(pwd)/Build/windows/${project}.exe"  \
