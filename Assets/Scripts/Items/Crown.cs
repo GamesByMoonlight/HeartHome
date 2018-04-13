888d3a388e1b58c722ac1bf67fda5fbe3847183c
LAPTOP-NHNQ0FS3\cisco <ciscom1971@yahoo.com>
2018-04-12 20:36:31 -0500

    feature/32

---
+++ Assets/Scripts/Items/Crown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crown : Item {
+
+    private PlayerAction playerAction;
+
+
+    void Awake()
+    {
+        playerAction = GameObject.Find("Player").GetComponent<PlayerAction>();
+
+        if (playerAction == null)
+        {
+            Debug.LogError("PlayerAction not found in scene. Hoe needs it");
+        }
+
+
+    }
+
+    public override void UseAt(GameObject location)
+    {
+
+
+        var soil = location.GetComponent<FertileSoil>();
+        if (soil != null)
+        {
+            soil.PlantSeeds();
+
+        }
+    }
+
+}
