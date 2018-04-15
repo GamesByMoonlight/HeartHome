d13400ecd18231617a81452ad6f7fc2b253b8b78
Scott Porter <scott.porter.ii@gmail.com>
2018-04-15 13:21:30 -0500

    Feature/8 - Heart sprites and animation (#78)
    * Created sandbox scene
    
    * Heartbeat and floating animations
    
    * Broken, Cursed and Frozen animations with controller setup
    
    * Heart behavior values modify with state change
    
    * Shivering added
    
    * Cleaned up and finalized scene
    
    * Heart Sprites and Animations finished

---
+++ Assets/Scripts/Heart/Shiver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shiver : MonoBehaviour {
+    [SerializeField]
+    float magnitude = 0.1f;
+    [SerializeField]
+    bool shivering = false;
+    public bool Shivering { get { return shivering; } set { SetShivering(value);} }
+
+    private void Start()
+    {
+        SetShivering(shivering);
+    }
+
+    void SetShivering(bool toShiver)
+    {
+        shivering = toShiver;
+        if(shivering && Application.isPlaying)
+        {
+            StartCoroutine(Shake());
+        }
+    }
+
+    IEnumerator Shake()
+    {
+        while(shivering)
+        {
+            transform.localPosition = (new Vector2(0f, 0f) + new Vector2(Random.Range(-magnitude, magnitude), Random.Range(-magnitude, magnitude)));
+            yield return new WaitForSeconds(.05f);
+        }
+        transform.position = new Vector2(0f, 0f);
+    }
+}
