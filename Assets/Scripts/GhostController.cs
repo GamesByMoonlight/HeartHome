cec43ed696d63d51f5130c0b200c4689f62c5515
saporter_mac <scott.porter.ii@gmail.com>
2018-04-18 22:01:59 -0500

    Waits at first flower before killing

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -6,11 +6,13 @@ public class GhostController : MonoBehaviour {
     public AnimationCurve FadeInCurve;
     public float FadeInTime = 3f;
     public float Speed = 3f;
+    public float KillFirstTime = 3f;
 
     Rigidbody2D rb;
     Animator animator;
     Flower target;
     SpriteRenderer spriteRenderer;
+    bool inspecting = true;
 
     private void Awake()
     {
@@ -43,7 +45,6 @@ public class GhostController : MonoBehaviour {
         {
             yield return null;
             spriteRenderer.color = new Color(1f, 1f, 1f, FadeInCurve.Evaluate((Time.time - time) / FadeInTime));
-            //Debug.Log(FadeInCurve.Evaluate((Time.time - time) / FadeInTime));
         }
         spriteRenderer.color = Color.white;
         Begin();
@@ -69,6 +70,18 @@ public class GhostController : MonoBehaviour {
         StartCoroutine(ChaseFlowers());
     }
 
+    IEnumerator InspectFirstFlower()
+    {
+        var movement = (target.transform.position - transform.position).normalized;
+        SetAnimator(Vector2.SignedAngle(Vector2.up, movement));
+        while(inspecting)
+        {
+            movement = (target.transform.position - transform.position).normalized;
+            rb.velocity = movement * (Speed / 2f);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
     // Update is called once per frame
     IEnumerator ChaseFlowers() {
         while (target)
@@ -85,6 +98,17 @@ public class GhostController : MonoBehaviour {
         rb.velocity = new Vector2(0f, 0f);
         StartCoroutine(WaitForFlowers());
 	}
+
+    IEnumerator KillFlowerInSeconds(Flower flower, float seconds)
+    {
+        float time = Time.time;
+        while(inspecting && Time.time - time < seconds)  // This coroutine could be called simultaneously.  The first one to finish should kill all
+        {
+            yield return null;
+        }
+        inspecting = false;
+        flower.Kill();
+    }
 
     Flower UpdateTargetFlower(Flower currentTarget)
     {
@@ -122,7 +146,16 @@ public class GhostController : MonoBehaviour {
     {
         var flower = collision.gameObject.GetComponent<Flower>();
         if (flower && flower.Alive)
-            flower.Kill();
+        {
+            if (inspecting)
+            {
+                StartCoroutine(KillFlowerInSeconds(flower, KillFirstTime));
+            }
+            else
+            {
+                flower.Kill();
+            }
+        }
     }
 
 }
