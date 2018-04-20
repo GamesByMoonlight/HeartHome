8fffd84bee28d4e7362ce3d1c281803b4a68d4a0
fregas <me@craigscode.com>
2018-04-19 20:40:00 -0500

    got log to burn for a while, then disappear

--- Assets/Scripts/Fireplace.cs
+++ Assets/Scripts/Fireplace.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FirePlace : MonoBehaviour {
+
+	public GameObject burningLog;
+	public int burnForSeconds = 5;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +20,17 @@ public class FirePlace : MonoBehaviour {
 
     public void Burn()
     {
+		burningLog.SetActive(true);
+	 	StartCoroutine(StopBurning());
         Debug.Log("the fire is so delightful");
     }
+
+     IEnumerator StopBurning()
+     {
+         yield return new WaitForSeconds(burnForSeconds);
+
+		 //the item is all done burning
+         burningLog.SetActive(false);
+     }	 
+
 }
