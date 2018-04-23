462145f22708776395d7d0a062a243f4d01dbb5b
saporter_mac <scott.porter.ii@gmail.com>
2018-04-22 22:06:59 -0500

    Area one exit added

---
+++ Assets/Scripts/Navigation/AreaOneExit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaOneExit : MonoBehaviour {
+    public string NextSceneName;
+
+    // Cursed Heart
+    void LeaveByChoice(HeartState heart)
+    {
+        heart.CurrentState = HeartState.HeartStateValues.Cursed;
+        SceneManager.LoadScene(NextSceneName);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var heartState = collision.gameObject.GetComponentInChildren<HeartState>();
+        if(heartState)
+        {
+            LeaveByChoice(heartState);
+        }
+    }
+}
