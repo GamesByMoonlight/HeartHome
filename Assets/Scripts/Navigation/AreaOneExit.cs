1737bccf9a2abc4e0f453fe5f542e85e4247ba31
saporter_mac <scott.porter.ii@gmail.com>
2018-04-22 22:40:29 -0500

    Fade transition added

--- Assets/Scripts/Navigation/AreaOneExit.cs
+++ Assets/Scripts/Navigation/AreaOneExit.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AreaOneExit : MonoBehaviour {
     public string NextSceneName;
+    public float FadeTime = 2f;
+    public FadeInOut Shade;
+
+    private void Awake()
+    {
+        if(Shade == null)
+            Debug.LogError("Shade is null.  Create a Panel with an Image UI and a FadeInOut script and reference it here for fading effect");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Shade.FadeIn(FadeTime));
+    }
 
     // Cursed Heart
     void LeaveByChoice(HeartState heart)
     {
         heart.CurrentState = HeartState.HeartStateValues.Cursed;
+        StartCoroutine(WaitForFade());
+    }
+
+    IEnumerator WaitForFade()
+    {
+        StartCoroutine(Shade.FadeOut(FadeTime));
+        yield return new WaitForSeconds(FadeTime);
         SceneManager.LoadScene(NextSceneName);
     }
 
---
+++ Assets/Scripts/Navigation/FadeInOut.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInOut : MonoBehaviour {
+    Image shade;
+
+	void Awake() {
+        shade = GetComponent<Image>();
+	}
+	
+    public IEnumerator FadeIn(float seconds)
+    {
+        var start = Time.time;
+        shade.color = new Color(0f, 0f, 0f, 1f);
+        while((Time.time - start) / seconds < 1f)
+        {
+            shade.color = new Color(0f, 0f, 0f, 1f - (Time.time - start) / seconds);
+            yield return null;
+        }
+        shade.color = new Color(0f, 0f, 0f, 0f); // just in case
+    }
+
+    public IEnumerator FadeOut(float seconds)
+    {
+        var start = Time.time;
+        shade.color = new Color(0f, 0f, 0f, 0f);
+        while ((Time.time - start) / seconds < 1f)
+        {
+            shade.color = new Color(0f, 0f, 0f, (Time.time - start) / seconds);
+            yield return null;
+        }
+        shade.color = new Color(0f, 0f, 0f, 1f); // just in case
+    }
+}
