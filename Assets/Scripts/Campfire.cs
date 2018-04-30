5b0b18ed08e45c02e0cef421a07548351a2fc456
saporter_mac <scott.porter.ii@gmail.com>
2018-04-30 18:42:21 -0500

    Campfire functionality

---
+++ Assets/Scripts/Campfire.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campfire : MonoBehaviour {
+    public Animator CampfireAnim;
+
+    bool entered = false;
+    bool started = false;
+    Coroutine checking;
+
+    private void Start()
+    {
+        StartCoroutine(CheckForBlaze());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        entered = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        entered = false;
+
+    }
+
+    IEnumerator CheckForBlaze()
+    {
+        while(!started)
+        {
+            if (entered && Input.GetButtonDown("Action"))
+            {
+                CampfireAnim.SetTrigger("Burn");
+                started = true;
+            }
+            yield return null;
+        }
+
+    }
+}
