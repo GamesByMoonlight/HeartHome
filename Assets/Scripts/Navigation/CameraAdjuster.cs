df387c17f9c627d01a1f0b011cf97aad62d9b008
saporter_mac <scott.porter.ii@gmail.com>
2018-04-29 22:54:40 -0500

    parallax and camera adjusting added

---
+++ Assets/Scripts/Navigation/CameraAdjuster.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAdjuster : MonoBehaviour {
+    public float MaxAdjust = 2.4f;
+    public float speed = .1f;
+
+    Camera mc;
+    Vector3 old;
+    float start;
+
+    private void Awake()
+    {
+        mc = Camera.main;
+    }
+
+    private void Start()
+    {
+        old = mc.transform.position;
+        start = MaxAdjust;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float diff = (old.y - mc.transform.position.y);
+
+
+        if(mc.transform.localPosition.y < MaxAdjust && diff < 0f)
+        {
+            mc.transform.position = mc.transform.position - new Vector3(0f, diff);
+        }
+        else if(mc.transform.localPosition.y > 0f && diff > 0f)
+        {
+            mc.transform.position = mc.transform.position - new Vector3(0f, diff);
+        }
+        old = mc.transform.position;
+	}
+}
---
+++ Assets/Scripts/Navigation/Parallax.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+    public float speed = 0.1f; // Higher the number, closer the background, faster the scrolling
+
+    Camera mc;
+    Vector3 old;
+
+    private void Awake()
+    {
+        mc = Camera.main;
+    }
+
+    private void Start()
+    {
+        old = mc.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 diff = (old - mc.transform.position) * speed;// * Time.deltaTime;
+        old = mc.transform.position;
+
+        transform.position = ((Vector2)transform.position + diff);
+    }
+}
