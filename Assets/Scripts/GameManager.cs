2227f361137ee8d14ca064e3bc425021270f369c
saporter_mac <scott.porter.ii@gmail.com>
2018-05-01 17:39:52 -0500

    Castle Scene interaction persists

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public bool PaintingMiniGamePlayed;// { get; set; }
+
+    private void Awake()
+    {
+        PaintingMiniGamePlayed = false;
+    }
+
+    //private void Start()
+    //{
+    //    SceneManager.sceneLoaded += OnSceneLoaded;
+    //}
+
+
+    //void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    //{
+
+    //}
+
+    //private void OnDisable()
+    //{
+    //    SceneManager.sceneLoaded -= OnSceneLoaded;
+    //}
+
+}
