4b58a728a98a0ba0b933c9af1afc1918c0db02aa
Lance <lance.bailey@gmail.com>
2018-06-22 18:24:25 -0500

    * Can swap between start and options screen * Not so sure why the buttons aren't fading in correctly - I'm not adding them to the list properly, maybe just punt and publicly expose * Need to figure out buttons to fade them in

--- Assets/Scripts/Navigation/StartupFadeController.cs
+++ Assets/Scripts/Navigation/StartupFadeController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartupFadeController : MonoBehaviour {
     public FadeInOut Shade;
     public CanvasGroup canvasGroup;
+    public Canvas StartScreenCanvas;
+    public Canvas OptionsScreenCanvas;
     public float FadeInTime = 1f;
     public float FadeOutTime = 1f;
     public string NextSceneName = "Area 1";
@@ -25,12 +28,19 @@ public class StartupFadeController : MonoBehaviour {
 
     public void OptionsScreen()
     {
-        StartCoroutine(FadeToOptions());
+        StartScreenCanvas.enabled = false;
+        OptionsScreenCanvas.enabled = true;
+    }
+
+    public void BackToStart()
+    {
+        StartScreenCanvas.enabled = true;
+        OptionsScreenCanvas.enabled = false;
     }
 
     public void EndGame()
     {
-        Application.Quit();
+        Application.Quit();  // This method is not deployable to all platforms (potential aproblems: Android, iPhone, webGL)
     }
 
     IEnumerator StartHelper()
@@ -44,13 +54,34 @@ public class StartupFadeController : MonoBehaviour {
     IEnumerator FadeInInstructions(float time)
     {
         var startTime = Time.time;
-        while(Time.time - startTime < time)
+
+        List<Button> buttons = new List<Button>();
+        List<Text> texts = new List<Text>();
+
+        GetComponentsInParent(true, buttons);
+        GetComponentsInParent(true, texts);
+
+        while (Time.time - startTime < time)
         {
             canvasGroup.alpha = (Time.time - startTime) / time;
+
+            foreach (Button eachButton in buttons)
+            {
+                Color buttonColor = eachButton.colors.normalColor;
+                buttonColor.a = (Time.time - startTime)/time;
+            }
+
+            foreach (Text eachText in texts)
+            {
+                Color textColor = eachText.color;
+                textColor.a = (Time.time - startTime) / time;
+            }
+
             yield return null;
         }
 
         canvasGroup.alpha = 1f;
+        
     }
 
     IEnumerator FadeToGame()
@@ -59,10 +90,5 @@ public class StartupFadeController : MonoBehaviour {
         yield return new WaitForSeconds(FadeOutTime);
         SceneManager.LoadSceneAsync(NextSceneName);
     }
-
-    IEnumerator FadeToOptions()
-    {
-        StartCoroutine(Shade.FadeOut(FadeOutTime));
-        yield return new WaitForSeconds(FadeOutTime);
-    }
+      
 }
