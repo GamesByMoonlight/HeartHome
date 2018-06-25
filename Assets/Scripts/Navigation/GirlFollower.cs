cd99b6b7338dcc7246377fc7d8a890809597ec9d
Lance <lance.bailey@gmail.com>
2018-06-25 17:02:15 -0500

    * Created darkness, girl walks in darkness with a small halo of light that follows her * Heart also has a halo * TODO - make the heart float off to the right of the girl to "show her the way"

---
+++ Assets/Scripts/Navigation/GirlFollower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlFollower : MonoBehaviour {
+
+    public Transform girl;
+	
+	// Update is called once per frame
+	void Update () {
+        if (girl)
+            transform.position = girl.transform.position;
+	}
+}
