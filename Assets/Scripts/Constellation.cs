03384b25468bcaae0a9073f633c0f7de38bc48df
Lance Bailey <37199007+LanceBailey@users.noreply.github.com>
2018-07-03 17:56:18 -0500

    #42 - Feature/star search (#146)
    * * Added buttons to start screen for Start Game, Options, and End Game
    * End Game ends game with application.quit
    * Other buttons not wired properly yet
    * Also broke the fade in of title
    
    * * Can swap between start and options screen
    * Not so sure why the buttons aren't fading in correctly - I'm not adding them to the list properly, maybe just punt and publicly expose
    * Need to figure out buttons to fade them in
    
    * * Start screen now has placeholders and nice fading of buttons
    
    * * Fixed canvas swapping bug
    
    * * Can't click buttons until fade in complete
    
    * * Added a new Scene 1, moved other scenes to new numbers
    
    * * Fixed castle scene loading to scene 4 instead of 3
    
    * * Created darkness, girl walks in darkness with a small halo of light that follows her
    * Heart also has a halo
    * TODO - make the heart float off to the right of the girl to "show her the way"
    
    * * Added star search minigame to final scene
    * Stars light up if they are valid
    * Removed functionality that starts game over
    * TODO Connect valid stars w/ line
    
    * *Star Search Minigame prefabs working
    
    * * Star Search isn't quite working out, but it's close
    * Committing so I can potentially fuck something up or fix it
    
    * * Refactored "Star Search Minigame" to watch for mouse clicks, and when a star is clicked to pass that information back to the Constellation class.

---
+++ Assets/Scripts/Constellation.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constellation : MonoBehaviour {
+
+    public GameObject MiniStar;
+    public float smallStarGap = 0.08f;
+
+    protected ClickableStar[] stars;
+
+    class Neighborstars
+    {
+        public Neighborstars(ClickableStar previousNeighbor, ClickableStar nextNeighbor)
+        {
+            PreviousNeighbor = previousNeighbor;
+            NextNeighbor = nextNeighbor;
+        }
+
+        public ClickableStar PreviousNeighbor { get; set; }
+        public ClickableStar NextNeighbor { get; set; }
+    }
+    
+	// Use this for initialization
+	void Start () {
+        stars = GetComponentsInChildren<ClickableStar>();
+	}
+	
+    public void UpdateConstellation(ClickableStar starClicked)
+    {
+        Constellation thisConstellation = starClicked.transform.parent.GetComponent<Constellation>();
+
+        //Debug.Log("starClicked - " + starClicked + " in thisConstellation - " + thisConstellation);
+        
+        Neighborstars neighborStars = GetNeighborStars(starClicked);
+        
+
+        if (IsValidNextStar(neighborStars) == true)
+        {   
+            starClicked.HasBeenFound = true;
+            starClicked.UpdateStatus();
+            ConnectStars(starClicked);
+
+        } else if (IsFirstStar(starClicked) == true)
+        {
+            
+            starClicked.HasBeenFound = true;
+            starClicked.UpdateStatus();
+        }
+        
+
+    }
+
+    bool IsValidNextStar(Neighborstars myNeighbors)
+    {
+
+
+        if (myNeighbors.PreviousNeighbor.HasBeenFound || myNeighbors.NextNeighbor.HasBeenFound)
+        {
+
+            return true;
+        }
+            
+
+        return false;
+    }
+
+    // Method to check the entire array of stars in this object and if no star has been activated, retur
+    bool IsFirstStar(ClickableStar starToCheck)
+    {
+        ClickableStar[] starArray = starToCheck.transform.parent.GetComponent<Constellation>().stars;
+
+        foreach (ClickableStar star in starArray)
+        {
+            if (star.HasBeenFound == true)
+                return false;
+        }
+
+        //Debug.Log("First Star in consteallation " + starToCheck.transform.parent);
+
+        return true;
+    }
+
+    void ConnectStars(ClickableStar starToCheck)
+    {
+        ClickableStar[] starArray = starToCheck.transform.parent.GetComponent<Constellation>().stars;
+
+        foreach (ClickableStar star in starArray)
+        {
+            Neighborstars myNeighbors = GetNeighborStars(star);
+            if (star.IsConnected == false && star.HasBeenFound == true && myNeighbors.NextNeighbor.HasBeenFound)
+            {
+                Vector3 startPoint = star.transform.position;
+                Vector3 endPoint = myNeighbors.NextNeighbor.transform.position;
+
+                Vector3 connectionLine = endPoint - startPoint;
+                float smallStarsNeeded = (connectionLine.magnitude / smallStarGap);
+
+                for (float i = 0; i <= smallStarsNeeded; i++)
+                {
+                    GameObject miniStar = Instantiate(MiniStar, startPoint + (connectionLine/smallStarsNeeded * i), Quaternion.identity);
+                    miniStar.transform.parent = star.transform;
+                }
+
+                star.IsConnected = true;
+            }
+        }
+
+        return;
+    }
+
+    Neighborstars GetNeighborStars(ClickableStar thisStar)
+    {
+        thisStar.IveBeenClicked = true;
+
+        int index = 0;
+
+        for (int i = 0; i < stars.Length; i++)
+        {
+            if (stars[i].IveBeenClicked == true)
+                index = i;
+        }
+
+        int previousNeighborIndex, nextNeighborIndex;
+
+        if (index == 0)
+        {
+            previousNeighborIndex = stars.Length - 1;
+            nextNeighborIndex = index + 1;
+        }
+        else if (index == stars.Length - 1)
+        {
+            previousNeighborIndex = index - 1;
+            nextNeighborIndex = 0;
+        }
+
+        else
+        {
+            previousNeighborIndex = index - 1;
+            nextNeighborIndex = index + 1;
+        }
+
+        thisStar.IveBeenClicked = false;
+
+        return (new Neighborstars(stars[previousNeighborIndex], stars[nextNeighborIndex]));
+    }
+
+       
+}
---
+++ Assets/Scripts/StarSearchMinigame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSearchMinigame : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ClickableStar clickedStar = CastRay();
+
+            if (clickedStar)
+            {
+                clickedStar.transform.parent.GetComponent<Constellation>().UpdateConstellation(clickedStar);
+            }
+        }
+    }
+
+    ClickableStar CastRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit2D objectClicked = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+
+        if (objectClicked.collider == null)
+            return null;
+
+        if (objectClicked.collider.gameObject.GetComponent<ClickableStar>() == null)
+            return null;
+
+        return objectClicked.collider.gameObject.GetComponent<ClickableStar>();
+    }
+}
