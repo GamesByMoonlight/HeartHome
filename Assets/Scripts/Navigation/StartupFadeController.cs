241e8260007911f7c5b04221b596f0bb0f038757
Lance Bailey <37199007+LanceBailey@users.noreply.github.com>
2018-07-03 13:33:57 -0500

    Feature/darkness scene (#145)
    * * Added buttons to start screen for Start Game, Options, and End Game
    * End Game ends game with application.quit
    * Other buttons not wired properly yet
    * Also broke the fade in of title
    
    * * Can swap between start and options screen
    * Not so sure why the buttons aren't fading in correctly - I'm not adding them to the list properly, maybe just punt and publicly expose
    * Need to figure out buttons to fade them in
    
    * * Start screen now has placeholders and nice fading of buttons
    
    * * Fixed canvas swapping bug
    
    * * Can't click buttons until fade in complete
    
    * * Added a new Scene 1, moved other scenes to new numbers
    
    * * Fixed castle scene loading to scene 4 instead of 3
    
    * * Created darkness, girl walks in darkness with a small halo of light that follows her
    * Heart also has a halo
    * TODO - make the heart float off to the right of the girl to "show her the way"

--- Assets/Scripts/Navigation/StartupFadeController.cs
+++ Assets/Scripts/Navigation/StartupFadeController.cs
@@ -2,58 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartupFadeController : MonoBehaviour {
     public FadeInOut Shade;
     public CanvasGroup canvasGroup;
+    public Canvas StartScreenCanvas;
+    public Canvas OptionsScreenCanvas;
     public float FadeInTime = 1f;
     public float FadeOutTime = 1f;
     public string NextSceneName = "Area 1";
 
-    bool startGame = false;
-
+    readonly List<Button> buttons = new List<Button>();
+    readonly List<Text> texts = new List<Text>();
+    
     // Use this for initialization
     private void Start()
     {
         StartCoroutine(StartHelper());
     }
+    
+    public void StartGame()
+    {
+        StartCoroutine(FadeToGame());
+    }
+
+    public void OptionsScreen()
+    {
+        OptionsScreenCanvas.enabled = true;
+        StartScreenCanvas.enabled = false;
+    }
+
+    public void BackToStart()
+    {
+        StartScreenCanvas.enabled = true;
+        OptionsScreenCanvas.enabled = false;
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();  // This method is not deployable to all platforms (potential aproblems: Android, iPhone, webGL)
+    }
 
     IEnumerator StartHelper()
     {
+        transform.parent.GetComponentsInChildren(true, buttons);
+        transform.parent.GetComponentsInChildren(true, texts);
+
         canvasGroup.alpha = 0f;
+
         StartCoroutine(Shade.FadeInWhite(FadeInTime));
         yield return new WaitForSeconds(FadeInTime);
         StartCoroutine(FadeInInstructions(FadeInTime));
-        StartCoroutine(WaitForAnyKey());
-
     }
 
     IEnumerator FadeInInstructions(float time)
     {
         var startTime = Time.time;
-        while(Time.time - startTime < time)
-        {
+        
+        while (Time.time - startTime < time)
+        { 
             canvasGroup.alpha = (Time.time - startTime) / time;
+
+            foreach (Button eachButton in buttons)
+            {
+                ColorBlock cb = eachButton.colors;
+                Color thisColor = cb.normalColor;
+                
+                thisColor.a = (Time.time - startTime) / time;
+                cb.normalColor = thisColor;
+
+                eachButton.colors = cb;
+            }
+
+            foreach (Text eachText in texts)
+            {
+                Color textColor = eachText.color;
+                textColor.a = (Time.time - startTime) / time;
+                eachText.color = textColor;
+            }
+            
             yield return null;
         }
-
+        
         canvasGroup.alpha = 1f;
+
+        Image thisImage = GetComponent<Image>();
+        thisImage.raycastTarget = false;
+
     }
 
-    IEnumerator WaitForAnyKey()
+    IEnumerator FadeToGame()
     {
-        while (!startGame)
-        {
-            if (Input.anyKey)
-            {
-                startGame = true;
-            }
-            yield return null;
-        }
-
         StartCoroutine(Shade.FadeOut(FadeOutTime));
         yield return new WaitForSeconds(FadeOutTime);
         SceneManager.LoadSceneAsync(NextSceneName);
     }
-
+      
 }
